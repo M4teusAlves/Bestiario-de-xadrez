5437a23eeb609ff8ba2bec244dceba65d8bb85cb
M4teusAlves <mateus.alves.desenvolvedor@gmail.com>
2022-07-16 11:06:05 -0300

    Adcionando conteúdo

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,6 @@
 
-let  i = 0;
+let  i = 0
+let button = document.getElementsByTagName("button")
 function openBook(){
     
     document.getElementById("title").style.display = "none"
@@ -8,6 +9,40 @@ function openBook(){
 }
 
 function page(){ 
+    let flag = 0
     document.getElementById("book").style.backgroundImage = `url(./assets/imagens/pagina.gif)`
     document.getElementsByTagName("section")[i].style.display = "flex"
+    if(i!==0){
+        button[0].style.display = "block"
+        flag++
+    }
+
+    if(i!==document.getElementsByTagName("section").length - 1){
+        button[1].style.display = "block"
+        flag++
+    }
+
+    if(flag<2){
+        button[0].style.marginRight = "97%"
+    }
+    
+}
+
+function nextPage(){
+    document.getElementsByTagName("section")[i].style.display = "none"
+    document.getElementById("book").style.backgroundImage = `url(./assets/imagens/nextpage.gif)`
+    button[0].style.display = "none"
+    button[1].style.display = "none"
+    i++
+    setTimeout(page,600)
+}
+
+function backPage(){
+    document.getElementsByTagName("section")[i].style.display = "none"
+    document.getElementById("book").style.backgroundImage = `url(./assets/imagens/backpage.gif)`
+    button[0].style.display = "none"
+    button[1].style.display = "none"
+    i--
+    setTimeout(page,600)
+
 }
