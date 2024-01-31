25800de6122efccf0164405a409cb660befa4d0a
M4teusAlves <mateus.alves.desenvolvedor@gmail.com>
2024-01-31 20:30:44 -0300

    Atualiza medidas e permite responsividade

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,9 +4,10 @@ let button = document.getElementsByTagName("button")
 function openBook(){
     
     document.getElementById("title").style.display = "none"
-    document.getElementById("book").style.backgroundImage = `url(./assets/imagens/fundo.gif)`
-    document.getElementsByTagName("main")[0].style.gap = "10%"
-    document.getElementsByTagName("main")[0].style.justifyContent = "flex-end"
+    document.getElementById("book").classList.remove('close')
+    document.getElementById("book").classList.add('open')
+    document.getElementById("botoes").style.display = "flex" 
+
     setTimeout(page,1900)
 }
 
@@ -14,18 +15,18 @@ function page(){
     let flag = 0
     document.getElementById("book").style.backgroundImage = `url(./assets/imagens/pagina.gif)`
     document.getElementsByTagName("section")[i].style.display = "flex"
-    if(i!==0){
-        button[0].style.display = "block"
-        flag++
-    }
+    
 
     if(i!==document.getElementsByTagName("section").length - 1){
-        button[1].style.display = "block"
+        document.getElementById("botoes").style.justifyContent = "flex-end" 
+        button[1].style.display = "block" 
         flag++
     }
 
-    if(flag<2){
-        button[0].style.marginRight = "97%"
+    if(i!==0){
+        button[0].style.display = "block"
+        document.getElementById("botoes").style.justifyContent = "space-between" 
+        flag++
     }
     
 }
